a44f2fbc1e1d9cb839f898a946462af5f97a1f22
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-02-13 22:21:44 +0900

    Update drawing_session_cog.py

--- cogs/drawing_session_cog.py
+++ cogs/drawing_session_cog.py
@@ -1,9 +1,7 @@
 # 기본적인 흐름과 상호작용에 대한 전반적인 로직
-
 from discord.ext import commands, ui
 from utils.drawbuttion import DrawButton
 from utils.options import option_1, option_2, option_3
-
 
 class DrawingSessionCog(commands.Cog):
     def __init__(self, bot):
@@ -19,14 +17,10 @@ class DrawingSessionCog(commands.Cog):
     
         self.in_use = True
     
-    
         view = ui.View()
-        view.add_item(option_1)
-        view.add_item(option_2)
-        view.add_item(option_3)
         
-    
-        await ctx.send("버튼을 눌러주세요! \n1번 : 여러 키워드 입력 후 하나 추첨 \n2번 : 유저 중에서 당첨자 추첨", view=view)
-    
-
+        view.add_item(DrawButton(ctx, view, label='1번', custom_id='draw_button_1', callback=None))
+        view.add_item(DrawButton(ctx, view, label='2번', custom_id='draw_button_2', callback=None))
+        view.add_item(DrawButton(ctx, view, label='3번', custom_id='draw_button_3', callback=None))  
           
+        await ctx.send("버튼을 눌러주세요! \n1번 : 여러 키워드 입력 후 하나 추첨 \n2번 : 유저 중에서 당첨자 추첨 \n3번 : 마법의 소라고동", view=view)
