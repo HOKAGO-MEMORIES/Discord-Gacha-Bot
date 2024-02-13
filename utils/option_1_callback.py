18bfb4cf978c2cb936318ebcff44ec8ebda852e3
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-02-14 01:14:55 +0900

    Update option_1_callback.py

--- utils/option_1_callback.py
+++ utils/option_1_callback.py
@@ -1,7 +1,8 @@
 # option_1 버튼에 대한 콜백함수 정의
 # 여러 키워드 입력 후 하나 추첨
 from discord.ext import ui
+import random
 
 async def on_option_1_click(interaction):
-
+    
     await interaction.response.send_message()
