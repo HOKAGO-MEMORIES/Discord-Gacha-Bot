83fb5c646611d5190a36c233089899948dceb289
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-07-18 22:12:22 +0900

    commit

--- cogs/team.py
+++ cogs/team.py
@@ -1,7 +1,23 @@
-# 두 번째 기능
-# 유저 중에서 당첨자 추첨
-from discord.ext import ui
+import discord
+from discord.ext import commands
 
-async def on_option_2_click(interaction):
+class Team(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    await interaction.response.send_message()
+    @commands.command(name='team')
+    async def team(self, ctx):
+        global current_user
+        if current_user is None:
+            current_user = ctx.author.id
+            try:
+                await ctx.send("Team 기능 실행 중")
+                # 팀 기능 로직을 여기에 추가
+            finally:
+                current_user = None  # 기능 완료 후 초기화
+        else:
+            await ctx.send(f'{ctx.author.mention}, 현재 다른 사용자가 team 기능을 사용 중입니다. 잠시 후 다시 시도해 주세요.')
+
+# Cog를 추가
+async def setup(bot):
+    await bot.add_cog(Team(bot))
--- cogs/vote.py
+++ cogs/vote.py
@@ -7,14 +7,14 @@ class Vote(commands.Cog):
 
     @commands.command(name='vote')
     async def vote(self, ctx):
-        global current_vote_user
-        if current_vote_user is None:
-            current_vote_user = ctx.author.id
+        global current_user
+        if current_user is None:
+            current_user = ctx.author.id
             try:
                 await ctx.send("Vote 기능 실행 중")
                 # 투표 기능 로직을 여기에 추가
             finally:
-                current_vote_user = None  # 기능 완료 후 초기화
+                current_user = None  # 기능 완료 후 초기화
         else:
             await ctx.send(f'{ctx.author.mention}, 현재 다른 사용자가 vote 기능을 사용 중입니다. 잠시 후 다시 시도해 주세요.')
 
