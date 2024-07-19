3d411cf23f95a37eb602dcb8e713341f0b249a30
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-07-19 23:05:18 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,61 @@
 import discord
-from discord import Game, Status
-from discord import app_commands
 import os
+import random
+import platform
+
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN') # 봇 토큰 
-GUILD_ID = int(os.getenv('GUILD_ID')) # 서버 ID
 
 intents = discord.Intents.default()
 intents.members = True 
 intents.message_content = True
 
-current_user = None # 현재 봇을 사용하고 있는 사람
-        
-class MyClient(discord.Client):
-    def __init__(self, *, intents: discord.Intents):
-        super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self)
 
-    async def setup_hook(self):
-        await self.tree.sync(guild=discord.Object(id=GUILD_ID))
-
-client = MyClient(intents=intents)
-        
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
-    print('------')
-    game = Game("ㅇㅇ")
-    await client.change_presence(status=Status.online, activity=game)
+class GachaBot(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix='/',
+            intents=intents,
+            help_command=commands.DefaultHelpCommand(),
+        )
     
+    async def load_cogs(self) -> None:
+        for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
+            if file.endswith(".py"):
+                extension = file[:-3]
+                try:
+                    await self.load_extension(f"cogs.{extension}")
+                    print(f"Loaded extension '{extension}'")
+                except Exception as e:
+                    exception = f"{type(e).__name__}: {e}"
+                    print(
+                        f"Failed to load extension {extension}\n{exception}"
+                    )
 
-# Cog 로드
-async def load_extensions():
-    await client.load_extension('cogs.vote')
-    await client.load_extension('cogs.team')
-    await client.load_extension('cogs.magic_conch')
+    # 봇 상태 무작위 변경 (game status task)
+    @tasks.loop(minutes=1.0)
+    async def status_task(self) -> None:
+        statuses = ["ㅇㅇ", "를!", "나가"]
+        await self.change_presence(activity=discord.Game(random.choice(statuses)))
+
+    # 봇 시작 전까지 상태 변경 대기 
+    @status_task.before_loop
+    async def before_status_task(self) -> None:
+        await self.wait_until_ready()
     
+    async def setup_hook(self) -> None:
+        print(f"Logged in as {self.user.name}")
+        print(f"discord.py API version: {discord.__version__}")
+        print(f"Python version: {platform.python_version()}")
+        print(
+            f"Running on: {platform.system()} {platform.release()} ({os.name})"
+        )
+        print("-------------------")
+        await self.load_cogs()
+        self.status_task.start()
 
-client.run(TOKEN)
+bot = GachaBot()
+bot.run(TOKEN)
